37a535bbddb616a93547d311bacc1e8ad409e16e
Kevin Viglucci <kviglucci@gmail.com>
2021-08-27 21:24:28 -0500

    refactor: move TcpDuplexConnection to its own file

--- UnityRSocket/Assets/Scripts/RSocket/TcpClientTransport.cs
+++ UnityRSocket/Assets/Scripts/RSocket/TcpClientTransport.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Sockets;
 using UnityEngine;
 
@@ -12,8 +10,6 @@ namespace RSocket
         private readonly int _port;
         private TcpClient _socket;
 
-        private static int DataBufferSize = 4096;
-
         public TcpClientTransport(string host, int port)
         {
             _host = host;
@@ -24,8 +20,8 @@ namespace RSocket
         {
             _socket = new TcpClient()
             {
-                ReceiveBufferSize = DataBufferSize,
-                SendBufferSize = DataBufferSize
+                ReceiveBufferSize = TcpDuplexConnection.DataBufferSize,
+                SendBufferSize = TcpDuplexConnection.DataBufferSize
             };
 
             _socket.BeginConnect(_host, _port, ar =>
@@ -47,68 +43,6 @@ namespace RSocket
                     Debug.LogError($"Failed to connect on TCP: {e}");
                 }
             }, _socket);
-        }
-
-        private class TcpDuplexConnection : IDuplexConnection
-        {
-            private readonly NetworkStream _stream;
-            private readonly byte[] _receiveBuffer;
-
-            public TcpDuplexConnection(TcpClient socket)
-            {
-                _receiveBuffer = new byte[DataBufferSize];
-                _stream = socket.GetStream();
-                _stream.BeginRead(_receiveBuffer, 0, DataBufferSize, OnData, null);
-            }
-
-            public void Handle()
-            {
-                throw new NotImplementedException();
-            }
-
-            private void OnData(IAsyncResult ar)
-            {
-                try
-                {
-                    int byteLength = _stream.EndRead(ar);
-                    if (byteLength <= 0)
-                    {
-                        return;
-                    }
-
-                    byte[] data = new byte[byteLength];
-                    Array.Copy(_receiveBuffer, data, byteLength);
-                    HandleData(data);
-
-                    _stream.BeginRead(_receiveBuffer, 0, DataBufferSize, OnData, null);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex);
-                }
-            }
-
-            private void HandleData(byte[] data)
-            {
-                throw new NotImplementedException();
-            }
-
-            public void Send(ISerializable<RSocketFrame.SetupFrame> frame)
-            {
-                try
-                {
-                    var serialized = frame.SerializeLengthPrefixed();
-
-                    _stream.BeginWrite(serialized.ToArray(), 0, serialized.Count, (ar) =>
-                    {
-                        _stream.EndWrite(ar);
-                    }, null);
-                }
-                catch (Exception ex)
-                {
-                    Debug.Log($"Error sending data to server via TCP: {ex}");
-                }
-            }
         }
     }
 }
