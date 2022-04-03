a0248d259314eb17b12f7acd14b78387e5bd7466
Kevin Viglucci <kviglucci@gmail.com>
2022-04-03 12:19:46 -0500

    refactor: move HasMetadataFlag to FrameUtils

--- UnityRSocket/Packages/io.rsocket/Runtime/Frame/FrameUtils.cs
+++ UnityRSocket/Packages/io.rsocket/Runtime/Frame/FrameUtils.cs
@@ -1,16 +1,21 @@
 namespace RSocket.Frame
 {
-    public class FrameUtils
+    public static class FrameUtils
     {
         public static bool IsConnectionFrame(RSocketFrame.AbstractFrame abstractFrame)
         {
             return abstractFrame.StreamId == 0;
         }
-        
+
         public static bool IsRequestFrame(RSocketFrame.AbstractFrame abstractFrame)
         {
             return FrameType.REQUEST_RESPONSE <= abstractFrame.Type
                    && abstractFrame.Type <= FrameType.REQUEST_CHANNEL;
+        }
+
+        public static bool HasMetadataFlag(ushort flags)
+        {
+            return (flags & (int)RSocketFlagType.METADATA) == (int)RSocketFlagType.METADATA;
         }
     }
 }
